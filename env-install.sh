0c54ecc65d3962a01dab43e7b89c0272e944ebdc
aucfan-igarashi <igarashi@gmail.com>
2015-12-10 21:40:26 +0900

    基本設定完了

--- env-install.sh
+++ env-install.sh
@@ -2,22 +2,21 @@
 
 BASEPATH=$(cd `dirname $0`; pwd)
 
-### ### install java
-### wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm
-### rpm -Uhv jdk-8u45-linux-x64.rpm
-### 
+### install java
+wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm
+# cd /develop/dev.es
+rpm -Uhv jdk-8u45-linux-x64.rpm
+
 ### sleep 5
 ### 
 ### ### check version java
 ### java -version
-### 
-### ### Download and install the public signing key
-### rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
-
+ 
+### Download and install the public signing key
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 
 file='/etc/yum.repos.d/es.repo'
 touch ${file}
-
 
 cat << __EOF__ >> ${file}
 [elasticsearch-1.7]
@@ -27,5 +26,16 @@ gpgcheck=1
 gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
 enabled=1 
 __EOF__
+
+
+### install elasticsearch
+yum install -y elasticsearch
+service        elasticsearch start
+chkconfig      elasticsearch on
+
+sleep 15
+
+curl -X GET http://localhost:9200
+
 
 exit
