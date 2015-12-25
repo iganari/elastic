51e568c2edcaef8f1951bf70c99ea8aca522be02
iganari <cigaguri@gmail.com>
2015-12-25 16:59:31 +0900

    .gitignoreのupdate

--- shell_insert-dummydata.sh
+++ shell_insert-dummydata.sh
@@ -12,5 +12,6 @@ curl -XPOST ${PRIVATEIP}:9200/test_index
 curl -XPUT  ${PRIVATEIP}:9200/test_index/2013/_mapping -d @mappings/nfl_mapping.json
 curl -XPOST ${PRIVATEIP}:9200/test_index/2013/_bulk --data-binary @datasets/nfl_2013.json > /dev/null
 
+cd ../
 rm -rf master.zip
 rm -rf Elasticsearch-datasets-master
