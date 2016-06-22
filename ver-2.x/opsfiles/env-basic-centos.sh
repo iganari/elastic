db0961b98bf3dea2a65dc990fd8a280384f58cb4
iganari <cigaguri@gmail.com>
2016-06-22 17:33:26 +0900

    kibanaを追加

---
+++ ver-2.x/opsfiles/env-basic-centos.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# create 2014/08/18
+
+# #### NOTE #####
+# running root !!
+# ###############
+
+# difie Directory
+
+basename=$(cd `dirname $0`; pwd)
+
+# stop unnecessary service
+service NetworkManager stop
+service iptables stop
+service ip6tables stop
+service kdump stop
+
+
+# cancel unnecessary autostart
+chkconfig iptables off
+chkconfig ip6tables off
+chkconfig kdump off
+
+
+# remove unnecessary autostart
+yum remove NetworkManager -y 
+
+# upgrade centos
+yum upgrade -y
+yum update bash -y
+
+# Installation of basic commands
+yum install vim wget openssl openssl-devel tree telnet nkf mlocate expect ntp net-snmp net-snmp-utils readline-devel readline-static dstat -y
+
+
+# Downloading and installing third-repository
+yum install yum-plugin-priorities -y
+yum install epel-release -y
+
+cd /tmp
+wget  http://rpms.famillecollet.com/enterprise/remi-release-6.rpm http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+rpm -ivh *.rpm
+
+# Rewriting of Priority
+cp -a /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.org
+sed -i "s/enabled=1/enabled=0/g" /etc/yum.repos.d/epel.repo
+sed -i "s/enabled = 1/enabled = 0/g" /etc/yum.repos.d/rpmforge.repo
+sed -i "19i\enabled = 1" /etc/yum.repos.d/CentOS-Base.repo
+sed -i "28i\enabled = 1" /etc/yum.repos.d/CentOS-Base.repo
+sed -i "37i\enabled = 1" /etc/yum.repos.d/CentOS-Base.repo
+
+# update third-repository
+yum --enablerepo=epel update epel-release -y 
+yum --enablerepo=remi update remi-release -y 
+yum --enablerepo=rpmforge update rpmforge-release -y 
+
+# Installation of basic commands from rpm
+yum install --enablerepo=epel tig iftop htop -y
+
+# setting date
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+# settinf ntpd service
+cp ${basename}/source/ntp.conf /etc/
+chmod 644 /etc/ntp.conf
+service ntpd restart
+chkconfig ntpd on
+
+
+# disable the ipv6
+echo "# ipv6 disable                        " >> /etc/sysctl.conf 
+echo "net.ipv6.conf.all.disable_ipv6 = 1    " >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+
+
+# modify SELinux
+### sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+setenforce 0
+
+
+# bash color custom
+echo 'export PS1="\[\033[1;34m\][\t][\u@\h \W]\\$\[\033[0m\] "' >> /etc/profile 
+
+
--- ver-2.x/opsfiles/env-install_es_2.x.sh
+++ ver-2.x/opsfiles/env-install_es_2.x.sh
@@ -2,24 +2,23 @@
 
 ### define
 BASEPATH=$(cd `dirname $0`; pwd)
-SCRPATH='/develop/dev.es'
+SCRPATH='/opt/dev.es'
 JDKFILE='jdk-8u45-linux-x64.rpm'
 
 ### install java
-if [ -f ${SCRPATH}/opsfiles/jdk/${JDKFILE} ]; then
+if [ -f ${SCRPATH}/tmp/jdk/${JDKFILE} ]; then
   ### when jdk exicts in local
   echo 'wget skip'
 else
   ### when jdk does not exist in local
   wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm
   # cp -f /home/vagrant/${JDKFILE} ${SCRPATH}/opsfiles/jdk/
-  cp -f ${JDKFILE} ${SCRPATH}/opsfiles/jdk/
+  cp -f ${JDKFILE} ${SCRPATH}/tmp/jdk/
   ### /home/vagrantに保存される
 fi
 
-cd ${SCRPATH}/opsfiles/jdk/
+cd ${SCRPATH}/tmp/jdk/
 rpm -Uhv ${JDKFILE}
-
 
 
 ### sleep 5
@@ -55,7 +54,9 @@ curl http://127.0.0.1:9200            # ver 2.x
 
 ### symlink
 mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bk
-cp -a /develop/dev.es/opsfiles/etc/elasticsearch/elasticsearch_2.x.yml /etc/elasticsearch/elasticsearch.yml
+cp -a ${SCRPATH}/etc/elasticsearch/elasticsearch_2.x.yml /etc/elasticsearch/elasticsearch.yml
+chown root.elasticsearch /etc/elasticsearch/elasticsearch.yml
+chmod 750                /etc/elasticsearch/elasticsearch.yml
 
 service elasticsearch restart
 
@@ -66,15 +67,30 @@ cd /usr/share/elasticsearch
 array=(
        'analysis-icu'
        'analysis-kuromoji'
+       'mobz/elasticsearch-head'
+       'royrusso/elasticsearch-HQ'
        'license'
        'marvel-agent'
        )
+
 
 for i in ${array[@]}
   do
     bin/plugin install ${i}
   done
 
+### append hosts
+cat << __EOF__ >> /etc/hosts
+192.168.33.21 deves01
+192.168.33.22 deves02
+192.168.33.23 deves03
+192.168.33.21 dedes01
+__EOF__
+
+
 service elasticsearch restart
+
+echo 'http://localhost:9200/_plugin/hq'
+echo 'http://localhost:9200/_plugin/head'
 
 exit
