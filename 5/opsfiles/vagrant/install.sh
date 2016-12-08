1a7e11c1ddae12536ed5b11b39cf317acd051f95
cigaguri <cigaguri@gmail.com>
2016-12-09 00:48:23 +0900

    インストールshellの修正

--- 5/opsfiles/vagrant/install.sh
+++ 5/opsfiles/vagrant/install.sh
@@ -5,7 +5,8 @@ apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
 apt-get update
 apt-get install -y elasticsearch
+/etc/init.d/elasticsearch start
 
-sleep 10
+sleep 20
 
 echo "curl http://127.0.0.1:9200"
